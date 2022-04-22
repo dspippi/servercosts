62b8fe72c7080aeff59e2b5b33b451a525e794b7
dspippi <103321598+dspippi@users.noreply.github.com>
2022-04-21 22:41:45 -0300

    Aula 21

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,4 +1,15 @@
-function NewProject(){
-    return(<h1>NewProject</h1>)
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+function NewProject() {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p><ProjectForm/></p>
+
+
+        </div>
+
+    )
 }
 export default NewProject
