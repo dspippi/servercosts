aaf8166fb44dd576b7eb10585d57e7291021cfab
dspippi <103321598+dspippi@users.noreply.github.com>
2022-04-23 19:23:38 -0300

    Aula 26: Início do Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,19 +1,33 @@
-import {useLocation} from 'react-router-dom'
+import styles from "../layout/Projects.module.css"
+import Container from "../layout/Container"
+import { useLocation } from 'react-router-dom'
 import Message from '../layout/Message'
+import LinkButton from '../layout/LinkButton'
+
+
 function Projects() {
-    
+
     const location = useLocation()
-    let message=""
-    if(location.state){
-        message=location.state.message
+    let message = ""
+    if (location.state) {
+        message = location.state.message
     }
-    
+
     return (
-    <div>
-        <h1>Meus Projetos</h1>
-        {message && <Message type="succes" msg={message} />}
-    </div>
-    
-        )
+        <div className={styles.project_container}>
+            <div class={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+
+            {message && <Message type="succes" msg={message} />}
+            
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+
+        </div>
+
+    )
 }
 export default Projects
